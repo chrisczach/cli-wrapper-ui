8310d3e41e93fbb73632418cb60fb2500fcdce9e
Chris Czach <czach@networkoptix.com>
2021-02-07 23:53:31 -0800

    working on getting logs on frontend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,35 +4,56 @@
 )]
 
 mod cmd;
+use tauri::api::{
+  config::get as get_config,
+  path::{resolve_path, BaseDirectory},
+};
+use std::io::prelude::*;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
+  let config = get_config()?;
+
+  let log_dir = resolve_path(
+    config.tauri.bundle.identifier.clone(),
+    Some(BaseDirectory::Cache),
+  )?;
+
+  if !log_dir.exists() {
+    std::fs::create_dir_all(&log_dir)?;
+  }
+
+  let path = log_dir.join("app.log");
+  if path.exists() {
+    std::fs::remove_file(&path)?;
+  }
+
+  let mut log = std::fs::File::create(path)?;
+
+  log.write_all(b"Hello Log\n")?;
+
   tauri::AppBuilder::new()
-    .invoke_handler(move |_webview, arg| {
+    .invoke_handler(|_webview, arg| {
       use cmd::Cmd::*;
       use std::process::Command;
       match serde_json::from_str(arg) {
-        Err(e) => {
-          Err(e.to_string())
-        }
+        Err(e) => Err(e.to_string()),
         Ok(command) => {
           match command {
             // definitions for your custom commands from Cmd here
             RunCommand { command } => {
               //  your command code
-              let mut shell = if cfg!(target_os = "windows") {
-                Command::new("cmd")
+              let exit = Command::new(if cfg!(target_os = "windows") {
+                "cmd"
               } else {
-                Command::new("sh")
-              };
+                "sh"
+              })
+              .args(vec!["-c", &command.to_string()])
+              .status()
+              .expect("error running command");
 
-              shell.arg("-c");
-              shell.arg(command);
-
-              let exit = shell.status().expect("error running command");
-              println!("process exited with: {}", exit);
               if exit.success() {
                 Ok(())
-            } else {
+              } else {
                 Err("Error running command".to_string())
               }
             }
@@ -42,4 +63,5 @@ fn main() {
     })
     .build()
     .run();
+    Ok(())
 }
